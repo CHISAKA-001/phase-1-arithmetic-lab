ffac62c0e677a09df348f9bb6f56a67f31aa0378
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-15 12:29:29 -0700

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+//multiply
+let multiply;
+multiply =num1 * num2
+    let num1 = 31;
+    let num2 = 2;
+    multiply = 62;
+
+//random int
+let random= Math.floor(Math.random() * 10) + 1;  
+
+//is an equation that calculates a remainder that is equal to 4
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+//will return 20 as the highest number in the set
+
+let max= Math.max(1,5,10,15,20);
